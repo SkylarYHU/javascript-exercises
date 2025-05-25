288e3e134f0f00b3c0e1a28ef73c7be513042419
Skylar <skylarhyn@gmail.com>
2025-05-25 15:44:11 +0100

    Complete exercise 06_repeatString

--- 06_repeatString/repeatString.js
+++ 06_repeatString/repeatString.js
@@ -1,6 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function(string, times) {
+  if(times <=0 ) return 'Error'
+  let fullString = ''
+  for(let i = 0; i < times; i++){
+    fullString += string
+  }
+  return fullString
 };
+
 
 // Do not edit below this line
 module.exports = repeatString;
