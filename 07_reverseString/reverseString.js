c1b5b63aad456b07ed2d03a691dac83ea5598072
Skylar <skylarhyn@gmail.com>
2025-05-25 15:53:55 +0100

    Complete exercise 07_reverseString

--- 07_reverseString/reverseString.js
+++ 07_reverseString/reverseString.js
@@ -1,6 +1,14 @@
-const reverseString = function() {
+// const reverseString = function(string) {
+//   let reversed = ''
+//   for(let i = string.length - 1; i >= 0; i--){
+//     reversed += string[i]
+//   }
+//   return reversed
+// };
 
-};
+const reverseString = function(string){
+  return string.split('').reverse().join('')
+}
 
 // Do not edit below this line
 module.exports = reverseString;
